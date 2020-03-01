9fe3178c5025b35f4046d7b76e18ebf885e31a3f
Stephen Romano <thestephenromano@gmail.com>
2020-03-01 17:58:48 -0500

    added simple RESTful interface

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-var addon = require('bindings')('node-quantlib.node');
+const express = require('express')
+const quantlib = require('bindings')('node-quantlib.node');
 
-console.log('Binomial: ', addon.binomial(
+const app = express()
+const port = 8081
+
+app.get('/optionprice/binomial', function (req, res) {
+
+    var evaluationDate = req.query.eval;
+    var settlementDate = req.query.settle;
+    var expirationDate = req.query.expire;
+    var optionType = req.query.t;
+    var strike = req.query.s;
+    var underlying = req.query.u;
+    var volatility = req.query.vol;
+    var riskfreeRate = req.query.rfr;
+    var dividendYield = req.query.div;
+
+    var price = quantlib.binomial(
+        evaluationDate,
+        settlementDate,
+        expirationDate,
+        optionType,
+        parseFloat(strike),
+        parseFloat(underlying),
+        parseFloat(volatility),
+        parseFloat(riskfreeRate),
+        parseFloat(dividendYield)
+    );
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(price)
+
+
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+/*
+console.log('Binomial: ', quantlib.binomial(
     "2/29/2020",    // evaluation date
     "3/2/2020",     // settlement date
     "3/20/2020",    // expiration date
@@ -11,7 +48,7 @@ console.log('Binomial: ', addon.binomial(
     .0097,          // risk free rate
     .019));         // dividend
 
-console.log('Monte Carlo: ', addon.montecarlo(
+console.log('Monte Carlo: ', quantlib.montecarlo(
     "2/29/2020",    // evaluation date
     "3/2/2020",     // settlement date
     "3/20/2020",    // expiration date
@@ -21,3 +58,4 @@ console.log('Monte Carlo: ', addon.montecarlo(
     .4011,          // volatility
     .0097,          // risk free rate
     .019));         // dividend
+ */
